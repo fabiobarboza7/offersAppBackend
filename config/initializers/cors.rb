fac2d0583ca533ffd9e7f709a15f0dd761b04f90
Fábio Souza <fabiobarboza7@hotmail.com>
2020-02-26 00:59:21 -0300

    wrong cors path

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://fake-cashback-api.herokuapp.com/'
+    origins 'https://fake-cashback-front.herokuapp.com/'
 
     resource '*',
       headers: :any,
